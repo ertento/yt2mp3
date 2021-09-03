38447a9e2fa329af92c04acfc6c9125002bfbb46
ertento <43725817+ertento@users.noreply.github.com>
2021-09-03 02:16:23 +0200

    Update yt2mp3.py
    added infinite while loop and a little function to check if the given link is valid or not.

--- yt2mp3.py
+++ yt2mp3.py
@@ -1,34 +1,42 @@
 from __future__ import unicode_literals
 import youtube_dl
-import keyboard
 import sys
-print("Insert: \n 1)Playlist \n 2)Single video \n q for exit")
-link = input ("")
-
-if link == "1":
-    ydl_opts = {
+while(True):
+    print("Insert: \n 1)for Playlist \n 2)for single video \n q for leave")
+    link = input ("")
+    if link == "1":
+        ydl_opts = {
+                'format': 'bestaudio/best',
+                'postprocessors': [{
+                    'key': 'FFmpegExtractAudio',
+                    'preferredcodec': 'mp3',
+                    'preferredquality': '320',
+                }],
+                'noplaylist': True,
+                
+            }
+    elif link == "2":
+        ydl_opts = {
             'format': 'bestaudio/best',
             'postprocessors': [{
                 'key': 'FFmpegExtractAudio',
                 'preferredcodec': 'mp3',
                 'preferredquality': '320',
             }],
-            'noplaylist': True,
-            
         }
-elif link == "2":
-    ydl_opts = {
-        'format': 'bestaudio/best',
-        'postprocessors': [{
-            'key': 'FFmpegExtractAudio',
-            'preferredcodec': 'mp3',
-            'preferredquality': '320',
-        }],
-    }
-elif link == "q" or "Q":
-    print("Leaving")
-    sys.exit(0)
-print("Insert link")
-link = input ("")
-with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-    ydl.download([link])
+    else:
+        print("Leaving")
+        sys.exit(0)
+    def is_supported(url):
+        extractors = youtube_dl.extractor.gen_extractors()
+        for e in extractors:
+            if e.suitable(url) and e.IE_NAME != 'generic':
+                return True
+        return False
+    print("Insert link")
+    link = input ("")
+    if not is_supported(link):
+        print("Invalid link")
+    else:
+        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+            ydl.download([link])
